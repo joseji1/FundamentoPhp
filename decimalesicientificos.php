936381baf9d4496423ee54b1d317b1388ef1b13a
Prefixx <joseji1@hotmail.com>
2016-06-03 14:33:52 +0200

    Numeros decimales y cientificos
    A la hora de definir los números decimales o científicos siempre utilizaremos el punto y no la coma
    5.432 ,0.123e2.

---
+++ decimalesicientificos.php
@@ -0,0 +1,17 @@
+<?php 
+/*Podemos poner dentro de las variables números enteros,decimales y científicos,lo 
+que tenemos que tener en cuenta es de utilizar el signo de punto ( .) y no la coma al definir 
+los números decimales y científicos  5.432 ( decimal utilizando el punto) 
+0.123e2( científico o exponencial de cómo flotante utilizando el punto y no la coma).
+*/
+$entero =5;
+echo $entero;
+echo "<br>";
+$decimal = 5.432;
+echo $decimal;
+echo"<br>";
+$cientifica= 0.123e2;
+echo $cientifica;
+
+
+?>
