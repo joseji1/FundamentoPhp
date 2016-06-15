ad7d2c2ab76d651cd9c44ba2779e72ace00b3d68
Prefixx <joseji1@hotmail.com>
2016-06-15 12:55:36 +0200

    Update Operadores-logicos.php

--- Operadores-logicos.php
+++ Operadores-logicos.php
@@ -17,8 +17,25 @@ echo ( $uno == 1 && $dos == 2) ; /* utilizamos el operador AND que con el doble 
                                     se utiliza el símbolo doble de ampersand &&*/
 $uno = 1;
 $dos = 2;
-echo ( $uno == 1 && $dos == 3) ; /* el resultado será Falso ya que solo se cumple la primera condición.*/
+echo ( $uno == 1 && $dos == 3) ; 
 
+//EL OPERTADOR OR
+/* el resultado será Falso ya que solo se cumple la primera condición.*/
+/*El operador OR  este operador determinará verdadero si solo una de las dos condiciones 
+es verdadera,para que sea verdadero solo se tiene que cumplir una o la otra de las condiciones
+.La expresion logica seria: “si x es verdadero o D es verdadero”,se utiliza la doble barra || .*/
+$tres = 3;
+$cuatro = 4;
+echo ($tres == 3 || $cuatro ==5);/* con esta sentencia nos dara como resultado Verdadero 
+        ya que una de las dos condiciones se cumple,esto es que la variable $tres es igual a 3,
+        la segunda variable no se cumple pero mientras una de las dos sea positiva la el resultado 
+        será true ( Verdadero). Uno de los Operandos es verdadero entonces será Verdadero el resultado*/
+
+//También podemos utilizar en los parametros los operadores de negación.
+
+$cinco = 5;
+$seis = 6;
+echo ($cinco !== $seis); // Verdadero ya que la variable $cinco no es igual que la variable $seis
 
 
 ?>
